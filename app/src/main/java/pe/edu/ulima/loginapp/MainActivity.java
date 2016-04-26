76159c3b9b440cabb54bbef1f5f1393ccebbe821
hernan <hquintana@devosinc.com>
2016-04-26 17:52:23 -0500

    Se agrego la funcionalidad de llamar activities de otras apps (correo y maps)

--- app/src/main/java/pe/edu/ulima/loginapp/MainActivity.java
+++ app/src/main/java/pe/edu/ulima/loginapp/MainActivity.java
@@ -1,8 +1,10 @@
 package pe.edu.ulima.loginapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import pe.edu.ulima.loginapp.beans.Usuario;
@@ -21,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
         String username = intentPasado.getStringExtra("username");
 
         tviMensaje.setText(String.format("Usuario: %s \n Password: %s", usuario.getUsuario(), usuario.getPassword()));
+    }
+
+    public void onVerMapaClicked(View view){
+        Uri location = Uri.parse("geo:0,0?q=Universidad de Lima");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        startActivity(mapIntent);
+    }
+
+    public void onMandarCorreo(View view){
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("text/plain");
+        //emailIntent.setData(Uri.parse("mailto:")); // Solo email apps se podrán ejecutar
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"hquintan@ulima.edu.pe"}); // TO
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Titulo del correo");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Contenido del texto");
+        // Si es que queremos enviar una imagen en el correo
+        //emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"));
+
+        startActivity(emailIntent);
     }
 }
